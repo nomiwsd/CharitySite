fe452273b2326b2671031cb0abe41d763a78626d
Muhammad Nouman <110379750+nomiwsd@users.noreply.github.com>
2024-01-05 07:14:30 +0500

    Complete Site Responsive

--- appsrc/views.py
+++ appsrc/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'index.html')
+    return render(request,'homepage.html')
 def ourcampaign(request):
     return render(request,'OurCampaign.html')
 def news(request):
--- static/js/script.js
+++ static/js/script.js
@@ -34,3 +34,33 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
     menuToggle.addEventListener("click", toggleMenu);
   });
+
+    // // Function to show the appropriate modal based on the dropdown selection
+    // function showSelectedModal() {
+    //   var dropdown = document.getElementById('paymentMethod');
+    //   var selectedOption = dropdown.options[dropdown.selectedIndex].value;
+  
+    //   if (selectedOption === 'card') {
+    //     showModal('cardModal');
+    //   } else if (selectedOption === 'bank') {
+    //     showModal('bankModal');
+    //   }
+    // }
+  
+    // // Function to show a modal by ID
+    // function showModal(modalId) {
+    //   var modal = document.getElementById(modalId);
+    //   modal.style.display = 'block';
+    // }
+  
+    // // Function to close a modal by ID
+    // function closeModal(modalId) {
+    //   var modal = document.getElementById(modalId);
+    //   modal.style.display = 'none';
+    // }
+  
+    // // Attach an event listener to the "Donate Now" button
+    // document.querySelector('.mainbtn').addEventListener('click', function() {
+    //   // Call the function to show the appropriate modal
+    //   showSelectedModal();
+    // });
